eb92ceefb3df9aa965da7bd1a2c30adb5cdc1382
Alejandro Seguí <alesegdia@gmail.com>
2015-12-12 15:38:08 +0100

    adding middle class Node for nodes

---
+++ core/src/com/alesegdia/virusex/entities/Node.java
@@ -0,0 +1,11 @@
+package com.alesegdia.virusex.entities;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+public class Node extends Entity {
+
+	public Node(float x, float y, Animation anim) {
+		super(x, y, anim);
+	}
+
+}
