2a57c0aa5999f7e98fdd32a28ae59b397b820330
Alejandro Seguí <alesegdia@gmail.com>
2015-12-12 15:38:38 +0100

    adding link class

---
+++ core/src/com/alesegdia/virusex/entities/Link.java
@@ -0,0 +1,20 @@
+package com.alesegdia.virusex.entities;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Link {
+
+	public Node nodeA;
+	public Node nodeB;
+	
+	public Link(Node nodeA, Node nodeB) {
+		this.nodeA = nodeA;
+		this.nodeB = nodeB;
+	}
+
+	public void render(ShapeRenderer srenderer)
+	{
+		srenderer.line(nodeA.position, nodeB.position);
+	}
+
+}
