ebb7490dae547835ea2e1ba177ac4b380ab5f62c
Alejandro Seguí <alesegdia@gmail.com>
2015-12-12 21:35:08 +0100

    adding PATHFIND to ToolMode

--- core/src/com/alesegdia/virusex/screen/ToolMode.java
+++ core/src/com/alesegdia/virusex/screen/ToolMode.java
@@ -1,5 +1,5 @@
 package com.alesegdia.virusex.screen;
 
 public enum ToolMode {
-	WEAK, MID, HARD, SPAWNER, START, GOAL, CONNECT, MOVE, DELETE, DISCONNECT
+	WEAK, MID, HARD, SPAWNER, START, GOAL, CONNECT, MOVE, DELETE, DISCONNECT, PATHFIND
 }
