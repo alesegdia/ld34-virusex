d4fbbe627f529a0483d27607073412fc00a1c27c
Alejandro Seguí <alesegdia@gmail.com>
2015-12-12 21:34:56 +0100

    tweaking launchers

--- desktop/src/com/alesegdia/virusex/desktop/DesktopEditorLauncher.java
+++ desktop/src/com/alesegdia/virusex/desktop/DesktopEditorLauncher.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.alesegdia.virusex.GdxGame;
 
-public class DesktopGameLauncher {
+public class DesktopEditorLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 600;
-		new LwjglApplication(new GdxGame(false), config);
+		new LwjglApplication(new GdxGame(true), config);
 	}
 }
