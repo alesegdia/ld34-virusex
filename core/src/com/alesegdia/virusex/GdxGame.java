b86e74b585f1d11c6f590eb3fc70c08bf015a241
Alejandro Seguí <alesegdia@gmail.com>
2015-12-12 15:39:52 +0100

    adding shape renderer and font to game class; initializing assets from there

--- core/src/com/alesegdia/virusex/GdxGame.java
+++ core/src/com/alesegdia/virusex/GdxGame.java
@@ -1,18 +1,29 @@
 package com.alesegdia.virusex;
 
+import com.alesegdia.virusex.assets.Gfx;
+import com.alesegdia.virusex.screen.GameScreen;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class GdxGame extends Game {
 	
 	public GameScreen gameScreen;
 	public SpriteBatch batch;
+	public BitmapFont font;
+	public ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
 		gameScreen = new GameScreen(this);
 		setScreen(gameScreen);
 		batch = new SpriteBatch();
+		font = new BitmapFont();
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setAutoShapeType(true);
+		
+		Gfx.Initialize();
 	}
 
 	@Override
