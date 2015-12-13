aa0898352f020aff31d96e8238c37d515a43a057
Alejandro Seguí <alesegdia@gmail.com>
2015-12-13 17:50:23 +0100

    antibody behaviour done

--- core/src/com/alesegdia/virusex/entities/organism/Antibody.java
+++ core/src/com/alesegdia/virusex/entities/organism/Antibody.java
@@ -1,13 +1,78 @@
 package com.alesegdia.virusex.entities.organism;
 
+import java.util.List;
+
 import com.alesegdia.virusex.World;
-import com.badlogic.gdx.graphics.g2d.Animation;
+import com.alesegdia.virusex.assets.Gfx;
+import com.alesegdia.virusex.entities.Faction;
+import com.alesegdia.virusex.entities.node.Node;
+import com.alesegdia.virusex.entities.node.SpawnerNode;
+import com.badlogic.gdx.math.Vector2;
 
 public class Antibody extends Organism {
 
-	public Antibody(World gw, float x, float y, Animation anim) {
-		super(gw, x, y, anim);
-		// TODO Auto-generated constructor stub
+	SpawnerNode spawner;
+	
+	public Antibody(SpawnerNode spawner, World gw, float x, float y) {
+		super(gw, x, y, Gfx.enemyAnim, Faction.ENEMY);
+		this.speedFactor = 0.7f;
+		this.spawner = spawner;
+	}
+	
+	@Override
+	public void update(float delta)
+	{
+		super.update(delta);
+		
+		if( gw.getVirus() != null && gw.getVirus().detected == true )
+		{
+			this.speedFactor = 0.7f;
+			this.recomputeSpeed();
+
+			if( state == IDLE_STATE )
+			{
+				List<Node> path = pf.findNextNode(this.getCurrentNode(), gw.getVirus().getCurrentNode());
+				if( path.size() < 2)
+				{
+					this.tryMove(gw.getVirus().targetNode);
+				}
+				else
+				{
+					this.tryMove(path.get(path.size()-2));
+				}
+			}
+		}
+		else
+		{
+			this.speedFactor = 5;
+
+			if( state == IDLE_STATE )
+			{
+				List<Node> path = pf.findNextNode(this.getCurrentNode(), spawner);
+				if( path.size() < 2 )
+				{
+					this.gw.removeAntibody(this);
+				}
+				else
+				{
+					this.tryMove(path.get(path.size()-2));
+				}
+				
+				if( this.getCurrentNode() instanceof SpawnerNode )
+				{
+					this.gw.removeAntibody(this);
+				}
+			}
+		}
+		
+		Vector2 v = new Vector2(this.position);
+		v.sub(gw.getVirus().position);
+		if( v.len() < 10 && !gw.getVirus().isStealth() )
+		{
+			this.gw.removeAntibody(this);
+			this.gw.getVirus().dealDamage(50);
+		}
+
 	}
 
 }
